502dc5ed8077eb11c41b5d4caf19480a454e8cd3
Bisera Naumovska <115948012+BiseraNaumovska@users.noreply.github.com>
2024-10-28 11:04:15 +0100

    Add files via upload

---
+++ p01_02/main.c
@@ -0,0 +1,13 @@
+/*
+ Program 1.2: p01_02.c
+Pechatenje edna linija so dve printf naredbi
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Dobro dojdovte");
+    printf(" vo C! \n");
+    return 0;
+}
