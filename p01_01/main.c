d5dedeed44842154cf4387a967a39f64ad44c382
Bisera Naumovska <115948012+BiseraNaumovska@users.noreply.github.com>
2024-10-28 11:03:30 +0100

    Add files via upload

---
+++ p01_01/main.c
@@ -0,0 +1,24 @@
+/*
+Program. 1.1: p01_01.c
+Prv program vo C
+*/
+
+// Da se praktikuvaat komentari dodeka se pishuvaat kodovi !
+
+#include <stdio.h> /* ovaa linija pretstavuva preprocesorska direktiva */
+/* site linii so # => preprocesorski, se izvrshuvaat pred da zapochne kompajliranjeto
+   ovaa linija mu kazuva na preprocesorot da ja vkluchi sodrzinata od standard input/output header(stdio.h) bibliotekata
+*/
+
+/*
+ Funkcijata main go zapochnuva izvrshuvanjeto na programot
+*/
+
+int main()
+{
+    printf("Dobro dojdovte vo C!\n"); // = naredba
+    printf("Sega sakam da iskoristam alarm! \a \n");
+    printf("Sega sakam da vmetnam kosa crta! \\ \n");
+    printf("\"Sega pishuvam vo navodnici\"\n");
+    return 0;   /* programot uspeshno zavrshil */
+}   /* kraj na funkcijata main */
